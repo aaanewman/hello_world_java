f16b1c3502d943c99699858f539786610a43d635
Anthony Newman <aaanewman@gmail.com>
2013-11-12 12:20:07 +1100

    Readme file

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Anthony
+ * Date: 8/11/13
+ * Time: 4:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+}
---
+++ tests/HelloWorldTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Anthony
+ * Date: 8/11/13
+ * Time: 5:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorldTest {
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
